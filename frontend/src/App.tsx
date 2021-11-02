7488c70c60b2d5b3f9ee2e08ed1b9df41121712e
samylly <samyllyh@gmail.com>
2021-11-02 14:33:02 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "componets/DataTable";
+import Footer from "componets/Footer";
+import NavBar from "componets/NavBar";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary"> olá mundo</h1>
+
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
